16d1ddc03217173555f282018ecf9365506970ab
Julien SEITE <julien.seite@cooperl.com>
2023-07-18 17:49:46 +0200

    chore: add logs to debug redis transport

--- redis_transport.go
+++ redis_transport.go
@@ -96,16 +96,20 @@ func NewRedisTransport(u *url.URL, l Logger, tss *TopicSelectorStore) (Transport
 }
 
 func subscribeToUpdate(t *RedisTransport) {
+	t.logger.Info("subscribeToUpdate:Subscribe")
 	pubsub := t.client.Subscribe(t.ctx, t.bucketName)
+	t.logger.Info("subscribeToUpdate:pubsub.Channel")
 	ch := pubsub.Channel()
 	for msg := range ch {
 		var update *Update
+		t.logger.Info("subscribeToUpdate:Unmarshal")
 		errUnmarshal := json.Unmarshal([]byte(msg.Payload), &update)
 		if errUnmarshal != nil {
 			t.logger.Error("error when unmarshaling message", zap.Any("message", msg), zap.Error(errUnmarshal))
 
 			continue
 		}
+		t.logger.Info("subscribeToUpdate:dispatch")
 		t.dispatch(update)
 	}
 }
@@ -113,6 +117,7 @@ func subscribeToUpdate(t *RedisTransport) {
 func getRedisLastEventID(ctx context.Context, client *redis.Client, bucketName string) string {
 	lastEventID := EarliestLastEventID
 
+	t.logger.Info("getRedisLastEventID:LIndex")
 	lastValue, err := client.LIndex(ctx, bucketName, 0).Result()
 	if err == nil {
 		var lastUpdate *Update
@@ -128,26 +133,32 @@ func getRedisLastEventID(ctx context.Context, client *redis.Client, bucketName s
 
 // Dispatch dispatches an update to all subscribers and persists it in Bolt DB.
 func (t *RedisTransport) Dispatch(update *Update) error {
+	t.logger.Info("Dispatch:select")
 	select {
 	case <-t.closed:
 		return ErrClosedTransport
 	default:
 	}
 
+	t.logger.Info("Dispatch:AssignUUID")
 	AssignUUID(update)
 
+	t.logger.Info("Dispatch:Lock")
 	t.Lock()
 	defer t.Unlock()
 
+	t.logger.Info("Dispatch:Marshal")
 	updateJSON, err := json.Marshal(*update)
 	if err != nil {
 		return fmt.Errorf("error when marshaling update: %w", err)
 	}
 
+	t.logger.Info("Dispatch:persist")
 	if err := t.persist(update.ID, updateJSON); err != nil {
 		return err
 	}
 
+	t.logger.Info("Dispatch:Publish")
 	// publish in pubsub for others mercure instances to consume the update and dispatch it to its subscribers
 	if err := t.client.Publish(t.ctx, t.bucketName, updateJSON).Err(); err != nil {
 		return fmt.Errorf("error when publishing update: %w", err)
@@ -158,17 +169,22 @@ func (t *RedisTransport) Dispatch(update *Update) error {
 
 // Called when a pubsub message is received.
 func (t *RedisTransport) dispatch(update *Update) error {
+	t.logger.Info("dispatch:select")
 	select {
 	case <-t.closed:
 		return ErrClosedTransport
 	default:
 	}
 
+	t.logger.Info("dispatch:Lock")
 	t.Lock()
 	defer t.Unlock()
 
+	t.logger.Info("dispatch:update")
 	for _, s := range t.subscribers.MatchAny(update) {
+		t.logger.Info("dispatch:Dispatch")
 		if !s.Dispatch(update, false) {
+			t.logger.Info("dispatch:Remove")
 			t.subscribers.Remove(s)
 		}
 	}
@@ -178,31 +194,40 @@ func (t *RedisTransport) dispatch(update *Update) error {
 
 // persist stores update in the database.
 func (t *RedisTransport) persist(updateID string, updateJSON []byte) error {
+	t.logger.Info("persist")
 	t.lastEventID = updateID
+	t.logger.Info("persist:LPush")
 	err := t.client.LPush(t.ctx, t.bucketName, updateJSON).Err()
 	if err != nil {
 		return fmt.Errorf("error while persisting to redis: %w", err)
 	}
+	t.logger.Info("persist:cleanup")
 
 	return t.cleanup()
 }
 
 // AddSubscriber adds a new subscriber to the transport.
 func (t *RedisTransport) AddSubscriber(s *Subscriber) error {
+	t.logger.Info("AddSubscriber:select")
 	select {
 	case <-t.closed:
 		return ErrClosedTransport
 	default:
 	}
 
+	t.logger.Info("AddSubscriber:Lock")
 	t.Lock()
+	t.logger.Info("AddSubscriber:Add")
 	t.subscribers.Add(s)
+	t.logger.Info("AddSubscriber:Unlock")
 	t.Unlock()
 
 	if s.RequestLastEventID != "" {
+		t.logger.Info("AddSubscriber:dispatchHistory")
 		t.dispatchHistory(s)
 	}
 
+	t.logger.Info("AddSubscriber:Ready")
 	s.Ready()
 
 	return nil
@@ -210,14 +235,17 @@ func (t *RedisTransport) AddSubscriber(s *Subscriber) error {
 
 // RemoveSubscriber removes a new subscriber from the transport.
 func (t *RedisTransport) RemoveSubscriber(s *Subscriber) error {
+	t.logger.Info("RemoveSubscriber:select")
 	select {
 	case <-t.closed:
 		return ErrClosedTransport
 	default:
 	}
 
+	t.logger.Info("RemoveSubscriber:Lock")
 	t.Lock()
 	defer t.Unlock()
+	t.logger.Info("RemoveSubscriber:Remove")
 	t.subscribers.Remove(s)
 
 	return nil
@@ -225,22 +253,27 @@ func (t *RedisTransport) RemoveSubscriber(s *Subscriber) error {
 
 // GetSubscribers get the list of active subscribers.
 func (t *RedisTransport) GetSubscribers() (string, []*Subscriber, error) {
+	t.logger.Info("GetSubscribers:RLock")
 	t.RLock()
 	defer t.RUnlock()
 
+	t.logger.Info("GetSubscribers:append")
 	var subscribers []*Subscriber
 	t.subscribers.Walk(0, func(s *Subscriber) bool {
 		subscribers = append(subscribers, s)
 
 		return true
 	})
+	t.logger.Info("GetSubscribers:return")
 
 	return t.lastEventID, subscribers, nil
 }
 
 func (t *RedisTransport) dispatchHistory(s *Subscriber) {
+	t.logger.Info("dispatchHistory:LRange")
 	updates, err := t.client.LRange(t.ctx, t.bucketName, 0, int64(t.size)).Result()
 	if err != nil {
+		t.logger.Info("dispatchHistory:HistoryDispatched")
 		s.HistoryDispatched(EarliestLastEventID)
 
 		return
@@ -250,6 +283,7 @@ func (t *RedisTransport) dispatchHistory(s *Subscriber) {
 	afterFromID := s.RequestLastEventID == EarliestLastEventID
 	for _, update := range updates {
 		var lastUpdate *Update
+		t.logger.Info("dispatchHistory:Unmarshal")
 		errUnmarshal := json.Unmarshal([]byte(update), &lastUpdate)
 		if errUnmarshal != nil {
 			s.HistoryDispatched(responseLastEventID)
@@ -267,6 +301,7 @@ func (t *RedisTransport) dispatchHistory(s *Subscriber) {
 			continue
 		}
 
+		t.logger.Info("dispatchHistory:Dispatch")
 		if !s.Dispatch(lastUpdate, true) {
 			s.HistoryDispatched(responseLastEventID)
 
@@ -281,6 +316,7 @@ func (t *RedisTransport) dispatchHistory(s *Subscriber) {
 
 // Close closes the Transport.
 func (t *RedisTransport) Close() (err error) {
+	t.logger.Info("Close")
 	t.closedOnce.Do(func() {
 		close(t.closed)
 
@@ -305,6 +341,7 @@ func (t *RedisTransport) Close() (err error) {
 
 // cleanup removes entries in the history above the size limit, triggered probabilistically.
 func (t *RedisTransport) cleanup() error {
+	t.logger.Info("cleanup:LLen")
 	sizeUpdates, errLen := t.client.LLen(t.ctx, t.bucketName).Result()
 	if errLen != nil {
 		return fmt.Errorf("error when getting updates length: %w", errLen)
@@ -317,6 +354,7 @@ func (t *RedisTransport) cleanup() error {
 		return nil
 	}
 
+	t.logger.Info("cleanup:LTrim")
 	errTrim := t.client.LTrim(t.ctx, t.bucketName, 0, int64(t.size)).Err()
 	if errTrim != nil {
 		return fmt.Errorf("error when trimming update length: %w", errLen)
